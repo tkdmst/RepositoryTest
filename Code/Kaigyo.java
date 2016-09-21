f440878302283f1bd6461b25c2a5175165ba2569
tkdmst <takada.misato@p-tec.jp>
2016-09-21 11:47:46 +0900

    Update Kaigyo.java

--- Code/Kaigyo.java
+++ Code/Kaigyo.java
@@ -4,11 +4,20 @@ public class Kaigyo {
 		System.out.printf("\n%n");
 
 		System.out.println("***printlnの場合***");
-		System.out.println("\n%n");
+		System.out.print("\n%n");
+		System.out.println();
+		
+		System.out.println("***タイプライター式改行***");
+		System.out.print("\r\n");
 	}
 }
 /*
 	初歩的なことなんだろうが疑問に思ったのでちょこっとテストを
 	printfだとどちらも改行になるが
 	printlnだと%nは上手く行かない
+
+	タイプライター式に\r\nとすると
+	普通の改行と同じになる
+	ただの\nだと↓に流すだけなのに対し、\r\nは最初に戻る動作をしている
+	(\r\n≒%n)
 */
