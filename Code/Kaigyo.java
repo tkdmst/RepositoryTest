e3177de9a1b87837c760dccf76f980f196675b4c
tkdmst <takada.misato@p-tec.jp>
2016-09-21 11:40:28 +0900

    Create Kaigyo.java

---
+++ Code/Kaigyo.java
@@ -0,0 +1,14 @@
+public class Kaigyo {
+	public static void main(String[] args) {
+		System.out.println("***printfの場合***");
+		System.out.printf("\n%n");
+
+		System.out.println("***printlnの場合***");
+		System.out.println("\n%n");
+	}
+}
+/*
+	初歩的なことなんだろうが疑問に思ったのでちょこっとテストを
+	printfだとどちらも改行になるが
+	printlnだと%nは上手く行かない
+*/
